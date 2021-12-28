6b2da47acf4bbe73a19fec18874a4e9eda8233d7
christopher pipitone <Chrispipitone123@gmail.com>
2021-12-28 16:53:54 -0500

    modified:   01-titleCaseEdit/titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,12 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  title = title.toLowerCase();
+  var words = title.split(' ');
+  for( var i = 0; i < words.length; i++)
+  {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+  }
+    return words.join(" ");
 }
-
 // Do not edit this line;
 module.exports = titleCaseEdit;
