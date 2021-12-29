85b5d3553c1da173cdf715e3eca80d02efd1e60e
christopher pipitone <Chrispipitone123@gmail.com>
2021-12-28 22:07:34 -0500

    modified:   17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js 	modified:   18-pairSum/pairSum.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  const myset = new Set();
+  for(let i = 0; i < args.length; i++)
+  {
+    for(let elem of args[i])
+    {
+      myset.add(elem);
+    }
+  }
+  return myset;
 }
 
 // Do not edit this line;
