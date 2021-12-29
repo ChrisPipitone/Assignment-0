de0aaecd6be4c5e21b83db8713d8bc7e6afcd56d
christopher pipitone <Chrispipitone123@gmail.com>
2021-12-28 22:18:43 -0500

    modified:   19-binarySearch/binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,3 +1,5 @@
+const { fa } = require("faker/lib/locales");
+
 class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
@@ -5,9 +7,19 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let middle = Math.floor(( nums.length / 2));
+
+    if(nums.length == 1 && nums[0] != target)
+      return false;
+
+    if(nums[middle] == target)
+      return true;
+    else if ( nums[middle] < target)
+      return this.binarySearch(nums.slice(middle), target);
+    else if( nums[middle] > target )
+      return this.binarySearch(nums.slice(0, middle), target);
   }
 }
-
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
